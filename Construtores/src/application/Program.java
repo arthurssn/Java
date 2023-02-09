bedf224a27e06969724f09d74be9f8ca03c9108d
arthurssn <arthurnovaes55@gmail.com>
2023-02-08 22:38:14 -0300

    adiciona construtor na class produto

---
+++ Construtores/src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter product data: \n");
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Price: ");
+        double price = scanner.nextDouble();
+
+        System.out.print("Quantity: ");
+        int quantity = scanner.nextInt();
+
+        Product product = new Product(name, price, quantity);
+
+        double totalValueInStock = product.totalValueInStock();
+
+        System.out.println("Name of product: " + product.name);
+
+        System.out.println("Price per product: " + product.price);
+
+        System.out.println("Total value in stock: " + totalValueInStock);
+
+        System.out.println("Quantity in stock: " + product.quantity);
+
+        product.addProducts(10);
+
+        product.removeProducts(2);
+
+        System.out.println("Quantity in stock: " + product.quantity);
+
+        System.out.println("Product: " + product);
+
+    }
+}
---
+++ Construtores/src/entities/Product.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double totalValueInStock() {
+        return this.quantity * this.price;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return String.format("Product data: %s, $ %.2f, %d units, Total: $ %.2f", this.name, this.price, this.quantity, this.totalValueInStock());
+    }
+}
