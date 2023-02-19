61c478781e25ae945eb5c086a859bc7fef861aa8
Arthur Novaes <arthurnovaes55@gmail.com>
2023-02-19 10:51:56 -0300

    fix: logica de media

--- ExerciciosFixacacao/src/application/EvenAverage.java
+++ ExerciciosFixacacao/src/application/EvenAverage.java
@@ -1,38 +1,33 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class EvenAverage {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
         System.out.print("Quantos elementos vai ter o vetor? ");
         int vectorLength = scanner.nextInt();
 
         int[] numbers = new int[vectorLength];
         int amountOfEvenNumbers = 0;
+        int sum = 0;
         for (int i = 0; i < vectorLength; i++) {
             System.out.print("Digite um numero: ");
             numbers[i] = scanner.nextInt();
             if (numberIsEven(numbers[i])) {
                 amountOfEvenNumbers++;
-            }
-        }
-        int[] evenNumbers = new int[amountOfEvenNumbers];
-        int sum = 0;
-        for (int i = 0; i < amountOfEvenNumbers; i++) {
-            if (numberIsEven(numbers[i])) {
-                evenNumbers[i] = numbers[i];
-                sum += evenNumbers[i];
+                sum += numbers[i];
             }
         }
 
-        if (evenNumbers.length == 0) {
+        double avg = (double) sum / (double) amountOfEvenNumbers;
+        if (amountOfEvenNumbers == 0) {
             System.out.println("NENHUM NUMERO PAR");
         } else {
-            double avg = (double) sum / amountOfEvenNumbers;
             System.out.printf("MEDIA DOS PARES = %.1f", avg);
         }
-
         scanner.close();
     }
 
